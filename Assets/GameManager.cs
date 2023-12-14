025eeaacabb12e9dc6c2737aae668356f5708a14
MARTY Grégoire <g.marty@rubika-edu.com>
2023-12-14 18:40:18 +0100

    Le Jeu

---
+++ Assets/GameManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Side_ activeSide;
+    public Side_ leftSide;
+    public Side_ rightSide;
+}
